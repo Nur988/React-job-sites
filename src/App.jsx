d870d20dbdbe4331990bc03c3b3b14ad020b8bf3
Nur988 <proteas988@gmail.com>
2025-01-27 01:01:34 +0600

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  
+  return (
+    <div>
+    <Navbar/>
+    <Hero/>
+    <HomeCards/>
+    </div>
+  )
+}
+
+export default App
